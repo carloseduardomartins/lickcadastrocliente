639e32bed0cdb8a71cd3e42d9f13b1c49868fb9c
Carlos Eduardo <carlosedrmartins@gmail.com>
2019-07-14 00:24:45 -0300

    habilitando botão cadastro

--- app/src/main/java/com/br/fernandocsrmartins/testecadu/MainActivity.java
+++ app/src/main/java/com/br/fernandocsrmartins/testecadu/MainActivity.java
@@ -2,13 +2,27 @@ package com.br.fernandocsrmartins.testecadu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.container_main);
+        setContentView(R.layout.seller_register_activity);
+
+        btn_cadastrar = findViewById(R.id.btn_cadastrar);
+
+        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setContentView(R.layout.container_main);
+            }
+        });
 
     }
 }
